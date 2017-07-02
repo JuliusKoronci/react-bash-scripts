5f77232d331c850c8365e683b4a1c764b535c36c
Julius Koronci <jk@web-solutions.sk>
2017-07-02 21:59:13 +0200

    removing undersocres from mapDispatch and mapState

--- build/templates/containerTemplate.js
+++ build/templates/containerTemplate.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 var getTemplate = function getTemplate(name) {
-	return "// @flow\nimport React, { Component} from 'react';\nimport PropTypes from 'prop-types';\nimport { connect } from 'react-redux';\nimport { bindActionCreators } from 'redux';\nimport " + name + "View from '../components/" + name + "';\nimport {} from '../actions/" + name + "Actions';\n\nexport class " + name + " extends Component {\n\n\tstate = {};\n\n\trender() {\n\t\treturn (\n\t\t\t<" + name + "View />\n\t\t);\n\t}\n}\n\n\n" + name + ".defaultProps = {};\n" + name + ".propTypes = {};\n\nfunction _mapStoreToProps(state, ownProps) {\n\treturn {};\n}\nfunction _mapDispatchToProps(dispatch) {\n\treturn bindActionCreators({}, dispatch);\n}\n\nexport default connect(_mapStoreToProps, _mapDispatchToProps)(" + name + ");\n\n";
+	return "// @flow\nimport React, { Component} from 'react';\nimport PropTypes from 'prop-types';\nimport { connect } from 'react-redux';\nimport { bindActionCreators } from 'redux';\nimport " + name + "View from '../components/" + name + "';\nimport {} from '../actions/" + name + "Actions';\n\nexport class " + name + " extends Component {\n\n\tstate = {};\n\n\trender() {\n\t\treturn (\n\t\t\t<" + name + "View />\n\t\t);\n\t}\n}\n\n\n" + name + ".defaultProps = {};\n" + name + ".propTypes = {};\n\nfunction mapStoreToProps(state, ownProps) {\n\treturn {};\n}\nfunction mapDispatchToProps(dispatch) {\n\treturn bindActionCreators({}, dispatch);\n}\n\nexport default connect(mapStoreToProps, mapDispatchToProps)(" + name + ");\n\n";
 };
 
 exports.default = getTemplate;
